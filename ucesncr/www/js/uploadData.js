99a3dc82051833ba9a68137d9e9d02f58860bc74
Nicola Critten <ucesncr@ucl.ac.uk>
2018-04-29 11:14:03 -0400

    Editing form layout.

--- ucesncr/www/js/uploadData.js
+++ ucesncr/www/js/uploadData.js
@@ -1,12 +1,15 @@
 function startDataUpload() {
-       alert ("start data upload");
-		var name = document.getElementById("name").value;
-		var surname = document.getElementById("surname").value;
-		var module = document.getElementById("module").value;
-		alert(name + " "+ surname + " "+module);
+	alert ("start data upload");
 
-		var postString = "name="+name +"&surname="+surname+"&module="+module;
-		processData(postString);
+	var name = document.getElementById("name").value;
+	var surname = document.getElementById("surname").value;
+	var module = document.getElementById("module").value;
+
+	alert(name + " "+ surname + " "+module);
+
+	var postString = "name="+name +"&surname="+surname+"&module="+module;
+
+	processData(postString);
 }
 
 var client;
@@ -15,14 +18,12 @@ function processData(postString) {
    client = new XMLHttpRequest();
    client.open('POST','http://developer.cege.ucl.ac.uk:30288/uploadData',true);
    client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-   client.onreadystatechange = dataUploaded;
+   client.onreadystatechange = dataUploaded;  
    client.send(postString);
 }
-// create the code to wait for the response from the data server, and process the
-response once it is received
+// create the code to wait for the response from the data server, and process the response once it is received
 function dataUploaded() {
-  // this function listens out for the server to say that the data is ready - i.e.
-has state 4
+  // this function listens out for the server to say that the data is ready - i.e. has state 4
   if (client.readyState == 4) {
     // change the DIV to show the response
     document.getElementById("dataUploadResult").innerHTML = client.responseText;
