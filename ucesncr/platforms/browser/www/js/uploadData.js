f5da01da0addbff41a9d8d8f18d5d19cde1ff649
Nicola Critten <ucesncr@ucl.ac.uk>
2018-05-07 12:38:52 -0400

    Commenting and formatting.

--- ucesncr/platforms/browser/www/js/uploadData.js
+++ ucesncr/platforms/browser/www/js/uploadData.js
@@ -1,39 +1,40 @@
+//This function checks that the fields have not been left empty
 function validateData() {
         var a=document.getElementById("pointname").value;
         var b=document.getElementById("question").value;
         var c=document.getElementById("answer1").value;
         var d=document.getElementById("answer2").value;
         var e=document.getElementById("answer3").value;
-        var f=document.getElementById("answer4").value;
-        var g=document.getElementById("answer1").value;
-        var h=document.getElementById("answer2").value;    
+        var f=document.getElementById("answer4").value;    
 
-        if (a==null || a=="",b==null || b=="",c==null || c=="",d==null || d=="")
+        if (a==null || a=="" || b==""|| c=="" || d=="" || e=="" || f == "")
         {
             alert("Please fill in all fields.");
             return false;
         }
         else
         {
-        	startDataUpload()
+        	startDataUpload();
         }
 }
 
+//This function gets the values from the question form text boxes and creates a string to be sent to the database
 function startDataUpload() {
 
+  //Gets the values from the textboxes
 	var pointname = document.getElementById("pointname").value;
 	var question = document.getElementById("question").value;
-
 	var answer1 = document.getElementById("answer1").value;
 	var answer2 = document.getElementById("answer2").value;
 	var answer3 = document.getElementById("answer3").value;
 	var answer4 = document.getElementById("answer4").value;
-
 	var lat = document.getElementById("lat").value;
 	var long = document.getElementById("long").value;
 
+  //Shows an alert telling the user what data is being sent to the database
 	alert("Submitting: " + pointname + "\n"+ question + " \n"+ answer1 + " " + answer2 + " " + answer3 + " " + answer4 + " \n" + lat + " " + long);
 
+  //Creates a string containing the question data
 	var postString = "pointname="+pointname +"&question="+question +"&answer1="+answer1 +"&answer2="+answer2 +"&answer3="+answer3+ "&answer4="+answer4;
 
 	// now get the radio button values
@@ -51,11 +52,15 @@ function startDataUpload() {
 	}
 
 	postString = postString + "&lat=" + lat + "&long=" + long;
+
+  //Calls the processData() function passing in the string of question data
 	processData(postString);
 }
 
+//Create a variable that will hold the XMLHttpRequest()
 var client;
 
+//A function that will send the data to the database using an XMLHttpRequest
 function processData(postString) {
    client = new XMLHttpRequest();
    client.open('POST','http://developer.cege.ucl.ac.uk:30288/uploadData',true);
@@ -64,9 +69,9 @@ function processData(postString) {
    client.send(postString);
 }
 
-// create the code to wait for the response from the data server, and process the response once it is received
+//A function to wait for the response from the data server, and process the response once it is received
 function dataUploaded() {
-  // this function listens out for the server to say that the data is ready - i.e. has state 4
+  //Listens out for the server to say that the data is ready - i.e. has state 4
   if (client.readyState == 4) {
     //Show an alert with the response text
     alert(client.responseText);
